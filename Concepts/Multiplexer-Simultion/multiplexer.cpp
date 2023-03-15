581faeb9ba5cefb85e1895e6a6032f4c54175fa5
Aayush Lamsal <102035433+Asura-zx@users.noreply.github.com>
2023-03-15 08:44:55 +0545

    Update multiplexer.cpp

--- Concepts/Multiplexer-Simultion/multiplexer.cpp
+++ Concepts/Multiplexer-Simultion/multiplexer.cpp
@@ -3,7 +3,7 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
-    const int nSignal=2; //no. of signal fed i.e n
+
     char andDiode(bool A, bool B, char diodes);//output and diodes that takes 3 inputs, 1 from i/p diode and n=2 from signal sources
                                             //and returns the data of diode only if both 2 input are false
     char orDiode(char w, char x, char y, char z);//or gate that gives the data of diode that has no false value
@@ -12,7 +12,7 @@ using namespace std;
         char diodes[]={'w','x','y','z'};//input diode i.e 2^n
         char diodeOp[4];//just a buffer to store diode output inside loop from line 16
         bool A,B,newA,newB; //signal input
-        int c=0;
+
         cout<<"type the bool input for signals A and B(0/1)"<<endl;
         cin>>A>>B;
         for(int i=1; i<=4; i++){
